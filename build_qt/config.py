8862fd0af71045c755f6f46186f646a8aa4b9467
Li-Yaosong <liyaosong1@qq.com>
2025-10-12 01:46:54 +0800

    [add]
    Signed-off-by: Li-Yaosong <liyaosong1@qq.com>

---
+++ build_qt/config.py
@@ -0,0 +1,256 @@
+
+import json
+import questionary
+import os
+from typing import Dict
+import platform
+import subprocess
+from build_qt.utils import detect_platform, download_component, extract_archive
+from build_qt.ohos_sdk_downloader import OhosSdkDownloader
+
+class Config:
+    config = None
+    user_config = None
+    def __init__(self, config_path: str):
+        self.root_path = os.path.abspath(os.path.dirname(config_path))
+
+        with open(config_path, 'r', encoding='utf-8') as f:
+            self.config = json.load(f)
+
+        plat = detect_platform()
+        self.ohos_sdk_downloader = OhosSdkDownloader(os_type=plat['osType'], os_arch=plat['osArch'], support_version=self.ohos_support_version())
+        self.init_user_config()
+        user_config_path = os.path.join(self.root_path, 'configure.json.user')
+        with open(user_config_path, 'r', encoding='utf-8') as f:
+            self.user_config = json.load(f)
+        self.perl_path = self.get_perl_path()
+        self.mingw_path = self.get_mingw_path()
+        self.ohos_sdk_path = self.get_ohos_sdk_path()
+
+
+    def init_user_config(self):
+        user_config_path = os.path.join(self.root_path, 'configure.json.user')
+        if not os.path.isfile(user_config_path):
+            questionary.print("用户配置文件 {} 不存在，开始配置。".format(user_config_path), style="bold italic fg:darkred")
+            answers = questionary.prompt([
+                {
+                    "type": "path",          # 提示类型（输入、选择、确认等）
+                    "name": "working-directory",      # 存储结果的键名
+                    "message": "请输入工作目录：",  # 显示给用户的提示信息
+                    "default": self.get_working_dir(),    # 默认值
+                },
+                {
+                    "type": "path",
+                    "name": "perl",
+                    "message": "请配置perl路径（默认则自动下载）：",
+                    "default": self.get_perl_path()
+                },
+                {
+                    "type": "path",
+                    "name": "mingw",
+                    "message": "请配置mingw路径（默认则自动下载）：",
+                    "default": self.get_mingw_path()
+                },
+                {
+                    "type": "path",
+                    "name": "ohos_sdk",
+                    "message": "请配置OpenHarmony SDK路径（默认则自动下载）：",
+                    "default": self.get_ohos_sdk_path()
+                },
+                {
+                    "type": "select",
+                    "name": "ohos_version",
+                    "message": "请选择OpenHarmony SDK版本：",
+                    "choices": self.ohos_sdk_downloader.get_supported_versions(),
+                    "default": str(self.ohos_version())
+                },
+                {
+                    "type": "select",
+                    "name": "build_qt_tag",
+                    "message": "请选择要编译的 Qt 版本：",
+                    "choices": [
+                        "v5.15.12-lts-lgpl",
+                        "v5.15.16-lts-lgpl",
+                        "v6.5.6-lts-lgpl"
+                    ],
+                    "default": self.tag()
+                }
+            ])
+            print("用户配置：", answers)
+            if answers == {}:
+                print("🛑 用户取消操作，程序退出。")
+                exit()   # 手动退出
+            else:
+                self.save_usr_config(answers)
+                print("用户配置已保存到 {}".format(user_config_path))
+
+    def dev_env_check(self):
+        system = platform.system()
+        need_perl = True
+        need_mingw = True
+        need_ohos_sdk = True
+        if system == "Windows":
+            if self.perl_path and os.path.isdir(self.perl_path):
+                result = subprocess.run([os.path.join(self.perl_path, "perl"), "-v"], capture_output=True, text=True)
+                if result.returncode == 0:
+                    print("perl 版本信息：")
+                    print(result.stdout)
+                    os.environ["PATH"] = os.environ.get("PATH", "") + os.pathsep + self.perl_path
+                    need_perl = False
+                else:
+                    print("perl 执行失败")
+            if self.mingw_path and os.path.isdir(self.mingw_path):
+                result = subprocess.run([os.path.join(self.mingw_path, "mingw32-make"), "--version"], capture_output=True, text=True)
+                if result.returncode == 0:
+                    print("mingw32-make 版本信息：")
+                    print(result.stdout)
+                    os.environ["PATH"] = os.environ.get("PATH", "") + os.pathsep + self.mingw_path
+                    need_mingw = False
+                else:
+                    print("mingw32-make 执行失败")
+        else:
+            need_perl = False
+            need_mingw = False
+        if self.ohos_sdk_path and os.path.isdir(self.ohos_sdk_path):
+            # 检查 native\oh-uni-package.json 是否存在
+            package_json_path = os.path.join(self.ohos_sdk_path, 'native', 'oh-uni-package.json')
+            print("OHOS SDK 路径：", package_json_path)
+            if os.path.isfile(package_json_path):
+                # 尝试读取 JSON 文件，检查是否能正确解析
+                try:
+                    import json
+                    with open(package_json_path, 'r', encoding='utf-8') as f:
+                        data = json.load(f)
+                    print("OHOS SDK 配置内容：")
+                    print(json.dumps(data, indent=2))
+                    os.environ["OHOS_SDK_PATH"] = self.ohos_sdk_path
+                    need_ohos_sdk = False
+                except Exception as e:
+                    print("警告: 无法解析 {}，文件可能损坏或格式不正确。错误: {}".format(package_json_path, e))
+        temp_dir = os.path.join(self.get_working_dir(), '.temp')
+        if need_perl and system == "Windows":
+            perl_url = self.config["dependencies"]["perl"]["url"]
+            perl_checksum = ('sha256', self.config["dependencies"]["perl"]["sha256"])
+            print("正在下载并安装 Perl...")
+            zip_path = download_component(perl_url, os.path.join(temp_dir, 'perl5.7z'), perl_checksum)
+            perl_extracted_path = os.path.join(self.get_working_dir(), 'perl')
+            extract_archive(zip_path, perl_extracted_path)
+            if os.path.isdir(perl_extracted_path):
+                self.perl_path = os.path.join(perl_extracted_path, 'bin')
+
+        if need_mingw and system == "Windows":
+            mingw_url = self.config["dependencies"]["mingw"]["url"]
+            mingw_checksum = ('sha256', self.config["dependencies"]["mingw"]["sha256"])
+            print("正在下载并安装 MinGW...")
+            zip_path = download_component(mingw_url, os.path.join(temp_dir, 'mingw64-x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z'), mingw_checksum)
+            mingw_extracted_path = os.path.join(self.get_working_dir(), 'mingw')
+            extract_archive(zip_path, mingw_extracted_path)
+            if os.path.isdir(mingw_extracted_path):
+                self.mingw_path = os.path.join(mingw_extracted_path, 'bin')
+
+        if need_ohos_sdk:
+            api_version = self.ohos_version()
+            print("正在下载并安装 OpenHarmony SDK...")
+            saved = self.ohos_sdk_downloader.download_component_by_name(api_version=api_version, component_name='native', dest_dir=temp_dir)
+            extract_archive(saved, self.ohos_sdk_path)
+
+        if need_perl or need_mingw or need_ohos_sdk:
+            self.dev_env_check()
+
+    def get_working_dir(self):
+        working_dir = self.config["config"]["working-directory"]
+        if "${pwd}" in working_dir:
+            working_dir = working_dir.replace("${pwd}", self.root_path)
+        working_dir = os.path.abspath(os.path.expanduser(working_dir))
+        return working_dir
+    
+    def get_perl_path(self):
+        if self.user_config is None:
+            _perl_path = self.config["config"]["perl"]
+        else:
+            _perl_path = perl_path = self.user_config.get("config", self.config.get("config")).get("perl", self.config["config"]["perl"])
+        if "${pwd}" in _perl_path:
+            _perl_path = _perl_path.replace("${pwd}", self.root_path)
+        _perl_path = os.path.abspath(os.path.expanduser(_perl_path))
+        return _perl_path
+
+    def get_mingw_path(self):
+        _mingw_path = self.config["config"]["mingw"]
+        if "${pwd}" in _mingw_path:
+            _mingw_path = _mingw_path.replace("${pwd}", self.root_path)
+        _mingw_path = os.path.abspath(os.path.expanduser(_mingw_path))
+        return _mingw_path
+    
+    def get_ohos_sdk_path(self):
+        _ohos_sdk_path = self.config["config"]["ohos_sdk"]
+        if "${pwd}" in _ohos_sdk_path:
+            _ohos_sdk_path = _ohos_sdk_path.replace("${pwd}", self.root_path)
+        if "${ohos_version}" in _ohos_sdk_path:
+            _ohos_sdk_path = _ohos_sdk_path.replace("${ohos_version}", str(self.ohos_version()))
+        _ohos_sdk_path = os.path.abspath(os.path.expanduser(_ohos_sdk_path))
+        return _ohos_sdk_path
+
+    def ohos_support_version(self):
+        return self.config["dependencies"]["ohos_sdk"]["support_version"]
+    
+    def ohos_version(self):
+        return self.config["config"]["ohos_version"]
+    
+    def qt_repo(self):
+        return self.config["repositories"]["qt_repo"]["url"]
+
+    def qt_ohos_patch_repo(self):
+        return self.config["repositories"]["qt-ohos-patch"]["url"]
+    
+    def tag(self):
+        return self.config["config"]["build_qt_tag"]
+
+    def build_type(self):
+        return self.config["config"]["build_type"]
+
+    def build_ohos_abi(self):
+        return self.config["config"]["build_ohos_abi"]
+
+    def get_config(self):
+        return self.config
+    
+    def save_usr_config(self, usr_config: Dict):
+        usr_config_path = os.path.join(self.root_path, 'configure.json.user')
+        obj = {
+            "config": usr_config
+        }
+        with open(usr_config_path, 'w', encoding='utf-8') as f:
+            json.dump(obj, f, ensure_ascii=False, indent=4)
+
+    def build_configure_options(self):
+        options = self.config["qt-config"]
+        result = []
+        if options["license"] in ["opensource", "commercial"]:
+            result.append("-{}".format(options['license']))
+        if options["confirm-license"]:
+            result.append("-confirm-license")
+        host_platform = "win32-g++"
+        if platform.system() == "Linux":
+            host_platform = "linux-g++"
+        elif platform.system() == "Darwin":
+            host_platform = "macx-clang"
+        result += ["-platform", host_platform]
+        result += ["-xplatform", options["-xplatform"]]
+        result += ["-opengl", options["-opengl"]]
+        if options["-opengles3"]:
+            result.append("-opengles3")
+        if options["-no-dbus"]:
+            result.append("-no-dbus")
+        if options["-disable-rpath"]:
+            result.append("-disable-rpath")
+        for nomake in options["-nomake"]:
+            result += ["-nomake", nomake]
+        skips = self.config[self.tag()]["-skip"]
+        for skip in skips:
+            result += ["-skip", skip]
+        result += ["-prefix", os.path.join(self.get_working_dir(), 'output', self.tag())]
+        result += ["-{}".format(self.build_type())]
+        result += ["-device-option", "OHOS_ARCH={}".format(self.build_ohos_abi())]
+        result += ["-make-tool", "mingw32-make -j{}".format(8) if platform.system() == "Windows" else "make -j{}".format(8)]
+        result += ["-verbose"]
+        return result
---
+++ build_qt/qt_repo.py
@@ -0,0 +1,216 @@
+"""qt_repo.py
+
+基于 GitPython 的 Qt 源码拉取与管理类
+
+功能：
+- 克隆（指定分支或 tag）
+- 支持克隆深度（depth）
+- 子模块初始化与更新（可递归、可浅克隆）
+- 切换/创建/删除分支
+- fetch/pull/reset_hard
+- 设置/查询远端 URL
+
+设计要点：
+- 使用 GitPython (git CLI 作为后端)，行为与系统 git 一致
+- 对于大型仓库（如 Qt），默认尽量使用浅克隆并在需要时按需更新子模块
+"""
+from typing import Optional, List
+import os
+import shutil
+import subprocess
+from git import Repo, GitCommandError
+
+class QtRepoError(Exception):
+    pass
+
+
+class QtRepo:
+    """用 GitPython 封装的仓库管理类。
+
+    参数：
+    - repo_path: 本地目标目录
+    - remote_name: 远端名，默认 origin
+    """
+    def __init__(self, repo_path: str, remote_name: str = 'origin'):
+        self.repo_path = os.path.abspath(repo_path)
+        self.remote_name = remote_name
+        self.repo = None
+        self.patch_repo = None
+
+        if os.path.isdir(os.path.join(self.repo_path, '.git')):
+            try:
+                self.repo = Repo(self.repo_path)
+            except Exception as e:
+                raise QtRepoError('打开仓库失败: {}'.format(e))
+
+    # ---------- 克隆相关 ----------
+    def clone(self, url: str, depth: int = 0, branch: Optional[str] = None) -> None:
+        """克隆仓库。
+
+        depth: 0 表示完整克隆；>0 表示使用 --depth
+        branch: 若指定，传递给 git clone 的 --branch
+        """
+        if os.path.exists(self.repo_path) and os.listdir(self.repo_path):
+            print('目录已存在: {}, 跳过克隆'.format(self.repo_path))
+            self.patch_repo = Repo(self.repo_path)
+            return 
+
+        git_exe = shutil.which('git')
+        if not git_exe:
+            raise QtRepoError('系统中未找到 git 可执行文件')
+
+        cmd = [git_exe, 'clone', '--recurse-submodules', '--single-branch', '--shallow-submodules']
+        if depth and depth > 0:
+            cmd += ['--depth', str(depth)]
+        if branch:
+            cmd += ['--branch', branch]
+        cmd += [url, self.repo_path]
+
+        try:
+            print('Cloning {} to {} with depth={}'.format(url, self.repo_path, depth))
+            subprocess.run(cmd, check=True)
+            self.repo = Repo(self.repo_path)
+            print('Clone succeeded. Remote URL: {}'.format(self.repo.remotes[self.remote_name].url))
+        except subprocess.CalledProcessError as e:
+            raise QtRepoError('git clone 失败: {}'.format(e))
+        print('Local branches: {}'.format(self.list_branches(local=True)))
+
+    def clone_patch_repo(self, url: str, depth: int = 0) -> None:
+        """克隆补丁仓库，位于主仓库同级目录的 repo_path + '_patch' 目录下。"""
+        patch_path = self.repo_path + '_patch'
+        if os.path.exists(patch_path) and os.listdir(patch_path):
+            print('目录已存在: {}, 跳过克隆'.format(patch_path))
+            self.patch_repo = Repo(patch_path)
+            return
+
+        git_exe = shutil.which('git')
+        if not git_exe:
+            raise QtRepoError('系统中未找到 git 可执行文件')
+
+        cmd = [git_exe, 'clone', '--single-branch']
+        if depth and depth > 0:
+            cmd += ['--depth', str(depth)]
+        cmd += [url, patch_path]
+
+        try:
+            print('Cloning patch repo {} to {} with depth={}'.format(url, patch_path, depth))
+            subprocess.run(cmd, check=True)
+            self.patch_repo = Repo(patch_path)
+            print('Patch repo clone succeeded. Remote URL: {}'.format(self.patch_repo.remotes[self.remote_name].url))
+        except subprocess.CalledProcessError as e:
+            raise QtRepoError('git clone 补丁仓库失败: {}'.format(e))
+    
+    def apply_patches(self, patch_dir: Optional[str] = None) -> None:
+        """应用补丁仓库中的补丁文件到主仓库。
+
+        patch_dir: 补丁文件所在目录，默认使用补丁仓库根目录
+        """
+        if not self.repo:
+            raise QtRepoError('主仓库未初始化')
+        if not self.patch_repo:
+            raise QtRepoError('补丁仓库未初始化')
+        self.reset_hard()
+        patch_dir = patch_dir or self.patch_repo.working_tree_dir
+        patch_dir = os.path.join(patch_dir, "patch", "v5.15.12")
+        if not os.path.isdir(patch_dir):
+            raise QtRepoError('补丁目录不存在: {}'.format(patch_dir))
+
+        patch_files = [f for f in os.listdir(patch_dir) if f.endswith('.patch')]
+        if not patch_files:
+            raise QtRepoError('补丁目录中没有 .patch 文件: {}'.format(patch_dir))
+
+        for patch_file in sorted(patch_files):
+            patch_path = os.path.join(patch_dir, patch_file)
+            if patch_file == "root.patch":
+                self.repo.git.apply(patch_path)
+            else:
+                try:
+                    module_repo = Repo(self.repo_path + '/' + patch_file.split('.')[0])
+                    module_repo.git.apply(patch_path, '--whitespace=nowarn')
+                    print('应用补丁 {} 成功'.format(patch_file))
+                except GitCommandError as e:
+                    raise QtRepoError('应用补丁 {} 失败: {}'.format(patch_file, e))
+        # 拷贝patch目录下的qtohextras到qt源码根目录
+        qtohextras_dir = os.path.join(patch_dir, "qtohextras")
+        if os.path.isdir(qtohextras_dir):
+            dest_dir = os.path.join(self.repo_path, "qtohextras")
+            if os.path.exists(dest_dir):
+                shutil.rmtree(dest_dir)
+            shutil.copytree(qtohextras_dir, dest_dir)
+            print('拷贝 qtohextras 目录成功')
+        print('所有补丁应用完成')
+    # ---------- 远端/fetch/pull ----------
+    def fetch(self, remote_name: Optional[str] = None) -> None:
+        if not self.repo:
+            raise QtRepoError('仓库未初始化')
+        try:
+            r = self.repo.remotes[remote_name or self.remote_name]
+            r.fetch()
+        except Exception as e:
+            raise QtRepoError('fetch 失败: {}'.format(e))
+
+    # ---------- 分支管理 ----------
+    def list_branches(self, local: bool = True, remote: bool = False) -> List[str]:
+        if not self.repo:
+            raise QtRepoError('仓库未初始化')
+        out = []
+        if local:
+            out += [h.name for h in self.repo.branches]
+        if remote:
+            out += [r.name for r in self.repo.remotes]
+        return out
+
+    def checkout(self, name: str) -> None:
+        if not self.repo:
+            raise QtRepoError('仓库未初始化')
+        try:
+            self.repo.git.checkout(name)
+        except Exception as e:
+            raise QtRepoError('checkout 失败: {}'.format(e))
+
+    # ---------- 重置 ----------
+    def reset_hard(self):
+        try:
+            # 1. 重置主仓库
+            git_exe = shutil.which('git')
+            if not git_exe:
+                raise QtRepoError('系统中未找到 git 可执行文件')
+            cmd = [git_exe, '-C', self.repo_path, 'reset', '--hard']
+
+            try:
+                subprocess.run(cmd, check=True)
+            except subprocess.CalledProcessError as e:
+                raise QtRepoError('重置主仓库 失败: {}'.format(e))
+            
+            cmd = [git_exe, '-C', self.repo_path, 'submodule', 'foreach', '--recursive', 'git', 'reset', '--hard']
+            try:
+                subprocess.run(cmd, check=True)
+            except subprocess.CalledProcessError as e:
+                raise QtRepoError('重置子仓库 失败: {}'.format(e))
+            self.clean()
+        except Exception as e:
+            raise QtRepoError('重置失败: {}'.format(e))
+        
+    def clean(self):
+        try:
+            # 1. 清理主仓库
+            git_exe = shutil.which('git')
+            if not git_exe:
+                raise QtRepoError('系统中未找到 git 可执行文件')
+            cmd = [git_exe, '-C', self.repo_path, 'clean' , '-fdx']
+            try:
+                subprocess.run(cmd, check=True)
+            except subprocess.CalledProcessError as e:
+                raise QtRepoError('清理主仓库 失败: {}'.format(e))
+            
+            cmd = [git_exe, '-C', self.repo_path, 'submodule', 'foreach', '--recursive', 'git', 'clean' , '-fdx']
+            try:
+                subprocess.run(cmd, check=True)
+            except subprocess.CalledProcessError as e:
+                raise QtRepoError('清理子仓库 失败: {}'.format(e))
+        except Exception as e:
+            raise QtRepoError('清理失败: {}'.format(e))
+
+
+if __name__ == '__main__':
+    print('This module provides QtRepo class using GitPython')
